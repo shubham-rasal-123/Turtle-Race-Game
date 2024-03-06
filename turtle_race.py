07f103c4a8fdd2d6fc1c7854bc690afb263d3d83
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-03-06 15:11:55 +0530

    added modified turtle race file

--- turtle_race.py
+++ turtle_race.py
@@ -4,7 +4,9 @@ import random
 is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+user_bet = screen.textinput(
+    title="Make your bet", prompt="Which turtle will win the race? Enter a color: "
+)
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
 all_turtles = []
@@ -22,7 +24,7 @@ if user_bet:
 
 while is_race_on:
     for turtle in all_turtles:
-        
+
         if turtle.xcor() > 230:
             is_race_on = False
             winning_color = turtle.pencolor()
